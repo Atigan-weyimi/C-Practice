05abf1dc63b1161b141e177fd8e016a00d5a1784
Atigan Weyimi <106789498+Atigan-weyimi@users.noreply.github.com>
2024-12-30 11:59:36 +0300

    Using oop in tic tac toe
    OOP

--- CPP Live/OPP.cpp
+++ CPP Live/OPP.cpp
@@ -1,65 +1,81 @@
 #include <iostream>
 using namespace std;
 
-// Function to draw the game board
-void drawBoard(char board[3][3]) {
-    cout << " " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl;
-    cout << "---+---+---" << endl;
-    cout << " " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl;
-    cout << "---+---+---" << endl;
-    cout << " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
-}
+// Enum for game status
+enum class GameStatus {
+    IN_PROGRESS,
+    X_WON,
+    O_WON,
+    DRAW
+};
 
-// Function to handle player move
-void playerMove(char board[3][3], char player) {
-    int row, col;
-    cout << "Enter row (1-3): ";2
-    cin >> row;
-    cout << "Enter column (1-3): ";
-    cin >> col;
-    row--; col--;
-    if (board[row][col] == ' ') {
-        board[row][col] = player;
-    } else {
-        cout << "Invalid move, try again." << endl;
-        playerMove(board, player);
-    }
-}
+// Class for a move
+class Move {
+public:
+    int i, j, piece;
 
-// Function to check for win
-bool checkWin(char board[3][3], char player) {
-    // Check rows
-    for (int i = 0; i < 3; i++) {
-        if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
-            return true;
+    Move(int i, int j, int piece) : i(i), j(j), piece(piece) {}
+
+    int getI() const { return i; }
+    int getJ() const { return j; }
+    int getPiece() const { return piece; }
+};
+
+// Class for the game board
+class Board {
+private:
+    int table[3][3];
+
+public:
+    Board() {
+        // Initialize the board with -1
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                table[i][j] = -1;
+            }
         }
     }
-    // Check columns
-    for (int i = 0; i < 3; i++) {
-        if (board[0][i] == player && board[1][i] == player && board[2][i] == player) {
-            return true;
-        }
-    }
-    // Check diagonals
-    if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
-        (board[0][2] == player && board[1][1] == player && board[2][0] == player)) {
-        return true;
-    }
-    return false;
-}
 
-int main() {
-    char board[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
-    char player = 'X';
-    while (true) {
-        drawBoard(board);
-        playerMove(board, player);
-        if (checkWin(board, player)) {
-            drawBoard(board);
-            cout << "Player " << player << " wins!" << endl;
-            break;
-        }
-        player = (player == 'X') ? 'O' : 'X';
+    // Method to set a cell on the board
+    void set(int i, int j, int piece) { table[i][j] = piece; }
+
+    // Method to get the value of a cell on the board
+    int get(int i, int j) const { return table[i][j]; }
+
+    // Method to display the board
+    void display() const {
+        cout << " " << (get(0, 0) == -1 ? " " : get(0, 0) == 0 ? "X" : "O")
+             << " | " << (get(0, 1) == -1 ? " " : get(0, 1) == 0 ? "X" : "O")
+             << " | " << (get(0, 2) == -1 ? " " : get(0, 2) == 0 ? "X" : "O") << endl;
+        cout << "---+---+---" << endl;
+        cout << " " << (get(1, 0) == -1 ? " " : get(1, 0) == 0 ? "X" : "O")
+             << " | " << (get(1, 1) == -1 ? " " : get(1, 1) == 0 ? "X" : "O")
+             << " | " << (get(1, 2) == -1 ? " " : get(1, 2) == 0 ? "X" : "O") << endl;
+        cout << "---+---+---" << endl;
+        cout << " " << (get(2, 0) == -1 ? " " : get(2, 0) == 0 ? "X" : "O")
+             << " | " << (get(2, 1) == -1 ? " " : get(2, 1) == 0 ? "X" : "O")
+             << " | " << (get(2, 2) == -1 ? " " : get(2, 2) == 0 ? "X" : "O") << endl;
     }
-    return 0;
-}
+
+    // Method to check for a winner
+    GameStatus checkWinner() const {
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if (table[i][0] != -1 && table[i][0] == table[i][1] && table[i][1] == table[i][2]) {
+                return table[i][0] == 0 ? GameStatus::X_WON : GameStatus::O_WON;
+            }
+        }
+
+        // Check columns
+        for (int j = 0; j < 3; j++) {
+            if (table[0][j] != -1 && table[0][j] == table[1][j] && table[1][j] == table[2][j]) {
+                return table[0][j] == 0 ? GameStatus::X_WON : GameStatus::O_WON;
+            }
+        }
+
+        // Check diagonals
+        if (table[0][0] != -1 && table[0][0] == table[1][1] && table[1][1] == table[2][2]) {
+            return table[0][0] == 0 ? GameStatus::X_WON : GameStatus::O_WON;
+        }
+        if (table[0][2] != -1 && table[0][2] == table[1][1] && table[1][1] == table[2][0]) {
+            return table[0][2] == 0 ? GameStatus::X_WON : GameStatus
